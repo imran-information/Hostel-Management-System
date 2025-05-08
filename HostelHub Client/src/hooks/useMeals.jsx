d3627ae0ee6a298ae961054d7f2badc50f818f42
imran <imranhossen0586@gmail.com>
2025-05-08 16:42:23 +0600

    meals page done

--- HostelHub Client/src/hooks/useMeals.jsx
+++ HostelHub Client/src/hooks/useMeals.jsx
@@ -2,12 +2,12 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const useMeals = (category) => {
+const useMeals = (category, mealsPage, priceFilter, sortOrder, searchQuery) => {
     const { isPending, error, data: meals } = useQuery({
-        queryKey: ['meals', category], // Include category in queryKey
+        queryKey: ['meals', category, mealsPage, priceFilter, sortOrder, searchQuery],
         queryFn: async () => {
             try {
-                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/meals?category=${category}`);;
+                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/meals?category=${category}&mealsPage=${mealsPage}&priceFilter=${priceFilter}&sortOrder=${sortOrder}&searchQuery=${searchQuery}`);;
                 return response.data;
             } catch (error) {
                 throw new Error(error.response?.data?.message || 'Failed to fetch meals');
