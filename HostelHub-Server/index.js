f5f20791510a9f6e26d28b2b0e076e9bad8f40df
imran hossain <imranme.global@gmail.com>
2025-05-17 18:48:53 +0600

    depoly

--- HostelHub-Server/index.js
+++ HostelHub-Server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://hostel-hub-beta.vercel.app'],
+    origin: ['http://localhost:5173', 'https://hostel-management-system-78bac.web.app'],
     credentials: true
 }));
 app.use(express.json());
